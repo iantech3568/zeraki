986741a573480cb24e895b31c475698004b788bd
iantech3568 <detogak@gmail.com>
2024-08-09 18:34:35 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+export const users = [
+    { email: 'teacher@example.com', password: 'teacher123', role: 'Teacher' },
+    { email: 'parent@example.com', password: 'parent123', role: 'Parent' },
+    { email: 'admin@example.com', password: 'admin123', role: 'Administrator' }
+];
+
+export const students = [
+    { name: 'John Doe', class: 'Grade 1', age: 6 },
+    { name: 'Jane Smith', class: 'Grade 2', age: 7 }
+];
+
+export const attendanceRecords = [];
+
+export const messages = [];
+
+export const results = [];
+
+export const supportTickets = [];
